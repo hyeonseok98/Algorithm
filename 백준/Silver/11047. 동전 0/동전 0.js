f8841bf69b87f7629987e399b4f150599e20f93f
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2024-01-29 11:18:13 +0900

    Update 동전 0.js

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,13 +1,13 @@
 let fs = require("fs");
 let input = fs.readFileSync("./dev/stdin").toString().split("\n");
 
-let [n, k] = input[0].split(" ");
-const arr = input.map(Number).slice(1);
+let [N, total] = input[0].split(" ");
+const coinType = input.map(Number).slice(1);
 
-let cnt = 0;
-for (let i = n - 1; i >= 0; i--) {
-  cnt += parseInt(k / arr[i]);
-  k %= arr[i];
+let count = 0;
+for (let i = N - 1; i >= 0; i--) {
+  count += parseInt(total / coinType[i]);
+  total %= coinType[i];
 }
 
-console.log(cnt);
+console.log(count);
