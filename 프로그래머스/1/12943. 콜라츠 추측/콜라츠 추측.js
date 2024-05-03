b0aa3022cd1c1c8a9a87672980c7d7b0097afd2f
Jamong <arozic425@gmail.com>
2024-05-03 20:56:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,10 @@
+function solution(num) {
+    let count = 0;
+    if(num === 1) return 0;
+    while(num !== 1) {
+        count++;
+        if(count === 500) return -1;
+        num % 2 === 0 ? num = num / 2 : num = num * 3 + 1 ;
+    }
+    return count
+}
