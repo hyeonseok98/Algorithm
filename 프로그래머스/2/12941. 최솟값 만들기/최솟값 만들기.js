8c4e3545ea36127ad0ef819d4197a6c78f898960
Jamong <arozic425@gmail.com>
2024-06-12 01:43:03 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.77 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,7 +1,7 @@
 function solution(A,B){
-    const sortedA = A.sort((a,b) => a - b);
-    const sortedB = B.sort((a,b) => b - a);
+    A.sort((a,b) => a - b);
+    B.sort((a,b) => b - a);
     
-    const answer = sortedA.reduce((acc, cur, idx) =>  acc + cur * sortedB[idx], 0);
+    const answer = A.reduce((acc, cur, idx) =>  acc + cur * B[idx], 0);
     return answer;
 }
