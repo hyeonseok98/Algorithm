4fa2adfcb1f8083b59b37725ab3aa52d0dcf9f72
Jamong <arozic425@gmail.com>
2025-01-10 23:49:16 +0900

    [level 0] Title: 할 일 목록, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,11 +1,3 @@
 function solution(todo_list, finished) {
-    const answer = [];
-    
-    todo_list.forEach((word, idx) => {
-        if(finished[idx] === false) {
-            answer.push(word);
-        }
-    })
-
-    return answer;
+    return todo_list.filter((_, idx) => !finished[idx])
 }
